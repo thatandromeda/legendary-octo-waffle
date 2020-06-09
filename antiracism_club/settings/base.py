9150eb5e17e2e8bfba1204c4c55a62309200d6f1
Andromeda Yelton <andromeda.yelton@gmail.com>
2020-06-08 21:10:07 -0400

    Get ready for Heroku

--- antiracism_club/settings/base.py
+++ antiracism_club/settings/base.py
@@ -13,19 +13,21 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ec$h9&3=sr*1_zy_r0pp8$wgjjnpacwhlamfs0oj@z0tv#(ad6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Application definition
 
@@ -75,18 +77,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'antiracism_club.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
---
+++ antiracism_club/settings/heroku.py
@@ -0,0 +1,89 @@
+import dj_database_url
+import os
+import sys
+
+from .base import *  # noqa
+
+
+# DATABASE CONFIGURATION
+# -----------------------------------------------------------------------------
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)  # noqa
+
+
+# SECURITY CONFIGURATION
+# -----------------------------------------------------------------------------
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT=True
+SESSION_COOKIE_SECURE=True
+CSRF_COOKIE_SECURE=True
+
+
+# ALLOWED_HOSTS CONFIGURATION
+# -----------------------------------------------------------------------------
+
+ALLOWED_HOSTS = ['antiracism-club-staging.herokuapp.com/',
+                 'antiracism-club.herokuapp.com']
+
+# This allows us to include review apps in ALLOWED_HOSTS even though we don't
+# know their name until runtime.
+APP_NAME = os.environ.get('HEROKU_APP_NAME')
+if APP_NAME:
+    url = '{}.herokuapp.com'.format(APP_NAME)
+    ALLOWED_HOSTS.append(url)
+
+
+# STATIC FILE CONFIGURATION
+# -----------------------------------------------------------------------------
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# whitenoise needs to be first
+MIDDLEWARE = ('whitenoise.middleware.WhiteNoiseMiddleware',) + MIDDLEWARE
+
+COMPRESS_ENABLED = True
+COMPRESS_OFFLINE = True
+
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# LOGGING CONFIGURATION
+# -----------------------------------------------------------------------------
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+    'formatters': {
+        'brief': {
+            'format': '%(asctime)s %(levelname)s %(name)s[%(funcName)s]: %(message)s',  # noqa
+        },
+    },
+    'handlers': {
+        'console_info': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'stream': sys.stdout
+        },
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console_info'],
+            'level': 'INFO',
+        }
+    }
+}
